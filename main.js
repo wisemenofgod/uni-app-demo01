ad18bdfbd560b7e7e6d98a4affbff3cbeabcab10
wisemenofgod <wtb033410@163.com>
2022-05-20 17:47:48 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+import {
+  $http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
+
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
