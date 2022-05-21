862f963a1cf5a6415e89a1d929412dba3ec913f5
wisemenofgod <wtb033410@163.com>
2022-05-21 18:22:58 +0800

    完成了商品列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,8 @@
 <template>
   <view>
-    goods_list
+    <view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+      <my-goods :goods="goods"></my-goods>
+    </view>
   </view>
 </template>
 
@@ -8,8 +10,74 @@
   export default {
     data() {
       return {
+        // 请求参数对象
+        queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类Id
+          cid: '',
+          // 页码值
+          pagenum: 1,
+          // 每页显示多少条数据
+          pagesize: 10
+        },
+        // 商品列表的数据
+        goodsList: [],
+        // 总数量，用来实现分页
+        total: 0,
 
-      };
+        //节流阀
+        isloading: false
+      }
+    },
+    methods: {
+      // 获取商品列表数据的方法
+      async getGoodsList(cb) {
+        this.isloading = true
+        // 发起请求
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        this.isloading = false
+        cb && cb()
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+
+
+      },
+      gotoDetail(item) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      },
+    },
+
+    onLoad(options) {
+      // 将页面参数转存到 this.queryObj 对象中
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    // 触底的事件
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+      if (this.isloading === true) return
+      // 让页码值自增 +1
+      this.queryObj.pagenum += 1
+      // 重新获取列表数据
+      this.getGoodsList()
+    },
+    //下拉刷新的事件
+    onPullDownRefresh() {
+      // 1. 重置关键数据
+      this.queryObj.pagenum += 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+
+      // 2. 重新发起请求
+      this.getGoodsList(() => uni.stopPullDownRefresh())
     }
   }
 </script>
